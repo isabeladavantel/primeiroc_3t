522fde5c4a6cb2fbe9fba2f5e4a31384f262b85d
isabeladavantel <110184962+isabeladavantel@users.noreply.github.com>
2022-09-29 12:49:20 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,8 @@
+let tabuada = 2;
+
+function escreva(){
+    document.write(tabuada + " x 1 = "+ (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = "+ (tabuada*2) + "<br>");
+    document.write(tabuada + " x 1 = "+ (tabuada*1) + "<br>");
+}
 
