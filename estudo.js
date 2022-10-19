decd07e245463b968ac539af60ee592b5a9b9271
isabeladavantel <110184962+isabeladavantel@users.noreply.github.com>
2022-10-19 19:49:58 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -401,6 +401,12 @@ function soma(){
     document.getElementById("resultado").innerHTML = "Soma: " + r;
 
 
-
-
 }
+    
+    function subtração(){
+    let n1 = document.getElementById("v1").value
+    let n2 = document.getElementById("v2").value
+    let n3 = document.getElementById("v3").value
+    let r = (Number(n1) - Number(n2) - Number(n3));
+    document.getElementById("resultado").innerHTML = "Subtração : " + r;
+ }
