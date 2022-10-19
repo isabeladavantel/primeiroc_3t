eb47f308b9b745c031692d45f0fbf4e08ea83181
isabeladavantel <110184962+isabeladavantel@users.noreply.github.com>
2022-10-19 19:47:46 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -391,8 +391,14 @@ function media(){
     let n3 = document.getElementById("v3").value;
     let r = (Number(n1) + Number(n2) + Number(n3));
     document.getElementById("resultado").innerHTML = "Media: " + r;
+}
 
-function 
+function soma(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let r = (Number(n1) + Number(n2) + Number(n3));
+    document.getElementById("resultado").innerHTML = "Soma: " + r;
 
 
 
